97044d98a0d2a06b8a113b1398b992f4ae7c737f
manel turki <menelturki@gmail.com>
2020-03-24 20:48:19 +0000

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,28 @@
-// Code ClickityClick Component Here
+import React, { Component } from 'react'
+
+export default class ClickityClick extends Component {
+    constructor(){
+        super();
+        this.state = {
+            toggled: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            return {
+            toggled: !previousState.toggled
+        }
+    })
+    } 
+   
+
+    render() {
+        return (
+            <div>
+            <p>I have {this.state.toggled ? 'on' : 'off'} been clicked!</p>
+            <button onClick= {this.handleClick}>Click me!</button>
+            </div>
+        );
+    }
+}
